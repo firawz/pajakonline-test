87a7cbcd6847c5e715bb11d424d852e15358f0a0
Ahmad Firaz <ahmadfiraz@gmail.com>
2025-06-22 11:09:12 +0700

    adjust analysis report to be able to accept xml as body and pdf from query params

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ import * as dotenv from "dotenv";
 import multer from "multer";
 import fs from "fs";
 import path from "path";
-import { readFileBase64 } from "./utils/readFileBase64.js";
 import pdf from 'pdf-parse/lib/pdf-parse.js';
-import jsQR from "jsqr";
-import { Jimp } from "jimp";
-import sharp from "sharp";
 import bodyParser from 'body-parser';
 import bodyParserXml from 'body-parser-xml';
-import { pdfToPng } from "pdf-to-png-converter";
-import { parsedDataMapping, parseText } from "./utils/data-mapping.js";
+import { parsedDataMapping, parseText, OBJtoXML } from "./utils/data-mapping.js";
 import { qrScanner } from "./utils/qr-code-scanner.js";
+import axios from "axios";
+
 
 dotenv.config();
 const upload = multer({ dest: "uploads/" });
@@ -115,102 +112,166 @@ router.post("/qr-validator", upload.single("file"), async (req, res) => {
   return res.status(200).json({ status: 'success', data: qr_code.data });
 })
 
-router.get("/analysis-report", async (req, res) => {
-  const data1 = { //xml
-    type: 'xml',
-    "kdJenisTransaksi": "07",
-    "fgPengganti": "0",
-    "Nomor Faktur": "0700002212345678",
-    "Tanggal Faktur": "01/04/2022",
-    "NPWP Penjual": "012345678012000",
-    "Nama Penjual": "PT ABC",
-    "alamatPenjual": "Jalan Gatot Subroto No. 40A, Senayan, Kebayoran Baru,\r\nJakarta Selatan 12910",
-    "NPWP Pembeli": "023456789217000",
-    "Nama Pembeli": "PT XYZ",
-    "alamatLawanTransaksi": "Jalan Kuda Laut No. 1, Sungai Jodoh, Batu Ampar,\r\nBatam 29444",
-    "Jumlah DPP": 15000000,
-    "Jumlah PPN": 1650000,
-    "jumlahPpnBm": "0",
-    "statusApproval": "Faktur Valid, Sudah Diapprove oleh DJP",
-    "statusFaktur": "Faktur Pajak Normal",
-    "referensi": "123/ABC/IV/2022",
-    "detailTransaksi": {
-      "nama": "KOMPUTER MERK ABC, HS Code 84714110",
-      "hargaSatuan": "5000000",
-      "jumlahBarang": "3",
-      "hargaTotal": "15000000",
-      "diskon": "0",
-      "dpp": "15000000",
-      "ppn": "1650000",
-      "tarifPpnbm": "0",
-      "ppnbm": "0"
+router.post("/analysis-report", async (req, res) => {
+  try {
+    // const bodyXml = `<resValidateFakturPm>
+    //       <kdJenisTransaksi>07</kdJenisTransaksi>
+    //       <fgPengganti>0</fgPengganti>
+    //       <nomorFaktur>0700002212345678</nomorFaktur>
+    //       <tanggalFaktur>01/04/2022</tanggalFaktur>
+    //       <npwpPenjual>012345678012000</npwpPenjual>
+    //       <namaPenjual>PT ABC</namaPenjual>
+    //       <alamatPenjual>Jalan Gatot Subroto No. 40A, Senayan, Kebayoran Baru,
+    //     Jakarta Selatan 12910</alamatPenjual>
+    //       <npwpLawanTransaksi>023456789217000</npwpLawanTransaksi>
+    //       <namaLawanTransaksi>PT XYZ</namaLawanTransaksi>
+    //       <alamatLawanTransaksi>Jalan Kuda Laut No. 1, Sungai Jodoh, Batu Ampar,
+    //     Batam 29444</alamatLawanTransaksi>
+    //       <jumlahDpp>15000000</jumlahDpp>
+    //       <jumlahPpn>1650000</jumlahPpn>
+    //       <jumlahPpnBm>0</jumlahPpnBm>
+    //       <statusApproval>Faktur Valid, Sudah Diapprove oleh DJP</statusApproval>
+    //       <statusFaktur>Faktur Pajak Normal</statusFaktur>
+    //       <referensi>123/ABC/IV/2022</referensi>
+    //       <detailTransaksi>
+    //         <nama>KOMPUTER MERK ABC, HS Code 84714110</nama>
+    //         <hargaSatuan>5000000</hargaSatuan>
+    //         <jumlahBarang>3</jumlahBarang>
+    //         <hargaTotal>15000000</hargaTotal>
+    //         <diskon>0</diskon>
+    //         <dpp>15000000</dpp>
+    //         <ppn>1650000</ppn>
+    //         <tarifPpnbm>0</tarifPpnbm>
+    //         <ppnbm>0</ppnbm>
+    //       </detailTransaksi>
+    //     </resValidateFakturPm>
+    //     `
+    const bodyXml = OBJtoXML(req?.body);
+    console.log(bodyXml, '<=== ini body xml apa?');
+    const dataXml = await axios.post('http://localhost:3000/api/v1/data', bodyXml, {
+      headers: {
+        'Content-Type': 'application/xml'
+      }
+    })
+
+    const response = await axios({
+      url: req?.query?.url,
+      method: 'GET',
+      responseType: 'stream'
+    });
+    const writer = fs.createWriteStream('temp.pdf');
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+    const dataPdf = await axios.post('http://localhost:3000/api/v1/validate-efaktur', {
+      file: fs.createReadStream('temp.pdf')
+    }, {
+      headers: { "Content-Type": "multipart/form-data" }
+    });
+
+    const data2 = dataPdf?.data?.data?.text
+    const data1 = dataXml?.data?.data
+    data1.type = 'xml'
+    data2.type = 'pdf'
+    // const data1 = { //xml
+    //   type: 'xml',
+    //   "kdJenisTransaksi": "07",
+    //   "fgPengganti": "0",
+    //   "Nomor Faktur": "0700002212345678",
+    //   "Tanggal Faktur": "01/04/2022",
+    //   "NPWP Penjual": "012345678012000",
+    //   "Nama Penjual": "PT ABC",
+    //   "alamatPenjual": "Jalan Gatot Subroto No. 40A, Senayan, Kebayoran Baru,\r\nJakarta Selatan 12910",
+    //   "NPWP Pembeli": "023456789217000",
+    //   "Nama Pembeli": "PT XYZ",
+    //   "alamatLawanTransaksi": "Jalan Kuda Laut No. 1, Sungai Jodoh, Batu Ampar,\r\nBatam 29444",
+    //   "Jumlah DPP": 15000000,
+    //   "Jumlah PPN": 1650000,
+    //   "jumlahPpnBm": "0",
+    //   "statusApproval": "Faktur Valid, Sudah Diapprove oleh DJP",
+    //   "statusFaktur": "Faktur Pajak Normal",
+    //   "referensi": "123/ABC/IV/2022",
+    //   "detailTransaksi": {
+    //     "nama": "KOMPUTER MERK ABC, HS Code 84714110",
+    //     "hargaSatuan": "5000000",
+    //     "jumlahBarang": "3",
+    //     "hargaTotal": "15000000",
+    //     "diskon": "0",
+    //     "dpp": "15000000",
+    //     "ppn": "1650000",
+    //     "tarifPpnbm": "0",
+    //     "ppnbm": "0"
+    //   }
+    // }
+
+    // const data2 = { //pdf 
+    //   type: 'pdf',
+    //   "NPWP Penjual": "012345678012000",
+    //   "Nama Penjual": "PT ABC",
+    //   "NPWP Pembeli": "0123456780121111111",
+    //   "Nama Pembeli": "PT XYZ",
+    //   "Nomor Faktur": "0700002212345678",
+    //   "Tanggal Faktur": "01/05/2025",
+    //   "Jumlah DPP": 15000000,
+    //   "Jumlah PPN": 1650000
+    // }
+
+    if (data1.length > data2.length) {
+      [data1, data2] = [data2, data1]
     }
-  }
 
-  const data2 = { //pdf 
-    type: 'pdf',
-    "NPWP Penjual": "012345678012000",
-    "Nama Penjual": "PT ABC",
-    "NPWP Pembeli": "0123456780121111111",
-    "Nama Pembeli": "PT XYZ",
-    "Nomor Faktur": "0700002212345678",
-    "Tanggal Faktur": "01/05/2025",
-    "Jumlah DPP": 15000000,
-    "Jumlah PPN": 1650000
-  }
+    const deviations = []
+    const validated_data = []
+    const type = data1.type
 
-  // let obj1 = Object.entries(data1)
-  // let obj2 = Object.entries(data2)
+    for (let [key, value] of Object.entries(data1)) {
+      const obj =
+      {
+        field: null,
+        pdf_value: null,
+        djp_api_value: null,
+        deviation_type: null,
+      }
 
-  if (data1.length > data2.length) {
-    [data1, data2] = [data2, data1]
-  }
+      const obj2 = {
+        [key]: value,
+      }
 
-  const deviations = []
-  const validated_data = []
-  const type = data1.type
-
-  for (let [key, value] of Object.entries(data1)) {
-    const obj =
-    {
-      field: null,
-      pdf_value: null,
-      djp_api_value: null,
-      deviation_type: null,
-    }
-
-    const obj2 = {
-      [key]: value,
-    }
-    // console.log(obj2, '<== ni apa?')
-    for (let [key1, value1] of Object.entries(data2)) {
-      if (key == key1) {
-        if (value != value1) {
-          obj.field = key
-          obj.djp_api_value = value
-          obj.pdf_value = value1
-          obj.deviation_type = "mismatch"
-          deviations.push(obj)
-        }
-        if (value == value1) {
-          validated_data.push(obj2)
+      for (let [key1, value1] of Object.entries(data2)) {
+        if (key == key1 && key != 'type') {
+          if (value != value1) {
+            obj.field = key
+            obj.djp_api_value = value
+            obj.pdf_value = value1
+            obj.deviation_type = "mismatch"
+            deviations.push(obj)
+          }
+          if (value == value1) {
+            validated_data.push(obj2)
+          }
         }
       }
+      if (key != 'type') {
+        if (type == 'xml') {
+          obj.djp_api_value = value
+          obj.deviation_type = "missing_in_pdf"
+        }
+        if (type == 'pdf') {
+          obj.pdf_value = value
+          obj.deviation_type = "missing_in_djp_api"
+        }
+        obj.field = key
+        deviations.push(obj)
+      }
     }
-    // console.log(key, value, '<== ini apa??')
-    if (type == 'xml') {
-      obj.djp_api_value = value
-      obj.deviation_type = "missing_in_pdf"
-    }
-    if (type == 'pdf') {
-      obj.pdf_value = value
-      obj.deviation_type = "missing_in_djp_api"
-    }
-    obj.field = key
-    deviations.push(obj)
+    const status = deviations.length > 0 ? 'validated_with_deviation' : deviations.length == 0 ? 'validated_successfully' : 'error'
+    return res.status(200).send({ status, message: 'Success comparing data', validation_results: { deviations, validated_data } })
+  } catch (error) {
+    // console.error(error, '<=== ini error apa?');
+    return res.status(500).send({ status: 'error', error: 'Internal Server Error' });
   }
-  const status = deviations.length > 0 ? 'validated_with_deviation' : deviations.length == 0 ? 'validated_successfully' : 'error'
-  return res.status(200).send({ status, message: 'Success comparing data', validation_results: { deviations, validated_data } })
 })
 
 
--- utils/data-mapping.js
+++ utils/data-mapping.js
@@ -94,3 +94,24 @@ export const parsedDataMapping = (data) => {
     }
     return result
 }
+
+export const OBJtoXML = (obj) => {
+    let xml = '';
+    for (let prop in obj) {
+        xml += obj[prop] instanceof Array ? '' : "<" + prop + ">";
+        if (obj[prop] instanceof Array) {
+            for (let array in obj[prop]) {
+                xml += "<" + prop + ">";
+                xml += OBJtoXML(new Object(obj[prop][array]));
+                xml += "</" + prop + ">";
+            }
+        } else if (typeof obj[prop] == "object") {
+            xml += OBJtoXML(new Object(obj[prop]));
+        } else {
+            xml += obj[prop];
+        }
+        xml += obj[prop] instanceof Array ? '' : "</" + prop + ">";
+    }
+    xml = xml.replace(/<\/?[0-9]{1,}>/g, '');
+    return xml
+}
